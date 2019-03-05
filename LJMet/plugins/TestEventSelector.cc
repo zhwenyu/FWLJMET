670af5b34fb6650cea0151b0f413322f35399105
rizki <rizki_syarif@brown.edu>
2019-03-05 15:11:26 -0600

    cleaning spaces/tabs

--- LJMet/plugins/TestEventSelector.cc
+++ LJMet/plugins/TestEventSelector.cc
@@ -11,11 +11,11 @@ class TestEventSelector : public BaseEventSelector {
 
 public:
 
-  
-    TestEventSelector();  
+
+    TestEventSelector();
     ~TestEventSelector();
 
-  
+
     // executes before loop over events
     virtual void BeginJob(std::map<std::string, edm::ParameterSet const > par);
 
@@ -23,7 +23,7 @@ public:
     virtual bool operator()( edm::EventBase const & event, pat::strbitset & ret);
 
     virtual void AnalyzeEvent( edm::EventBase const & event, LjmetEventContent & ec );
-    
+
     // executes after loop over events
     virtual void EndJob();
 
@@ -33,17 +33,17 @@ protected:
 
     // containers for config parameter values
     //std::map<std::string,std::string>    msPar;
-    
-    edm::EDGetTokenT<edm::TriggerResults> 		triggersToken;      
-	std::vector<std::string>					vTargetTrigs;
-	
-	bool										debug;
+
+    edm::EDGetTokenT<edm::TriggerResults> 		triggersToken;
+    std::vector<std::string>			        vTargetTrigs;
+
+    bool						debug;
 
 
 
 private:
 
-  
+
 
 };
 
@@ -62,22 +62,22 @@ TestEventSelector::~TestEventSelector()
 void TestEventSelector::BeginJob( std::map<std::string, edm::ParameterSet const> par)
 {
     std::cout << mLegend << "initializing Test selection" << std::endl;
-    
+
     debug = true;
 
     BaseEventSelector::BeginJob(par);
 
-	triggersToken		= consumes<edm::TriggerResults>("TriggerResults","","HLT");
-	//vTargetTrigs			= iConfig.getParameter<std::vector<std::string>>("HLTpaths");
-	vTargetTrigs.push_back("HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v");
-	vTargetTrigs.push_back("HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v");
-	vTargetTrigs.push_back("HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v");
-	vTargetTrigs.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v");
-  
+    triggersToken		= consumes<edm::TriggerResults>("TriggerResults","","HLT");
+    //vTargetTrigs			= iConfig.getParameter<std::vector<std::string>>("HLTpaths");
+    vTargetTrigs.push_back("HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v");
+    vTargetTrigs.push_back("HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v");
+    vTargetTrigs.push_back("HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v");
+    vTargetTrigs.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v");
+
     bFirstEntry = true; //in case anything needs a first entry bool.
-    
+
     //See "PhysicsTools/SelectorUtils/interface/EventSelector.h"
-    push_back("No selection");  
+    push_back("No selection");
     push_back("Trigger");
     push_back("All cuts");
 
@@ -86,77 +86,76 @@ void TestEventSelector::BeginJob( std::map<std::string, edm::ParameterSet const>
     set("Trigger",true);
     set("All cuts",true);
 
-    
-   
-} // end of BeginJob() 
+
+} // end of BeginJob()
 
 bool TestEventSelector::operator()( edm::EventBase const & event, pat::strbitset & ret)
 {
-	std::cout << "Processing Event in TestEventSelector::operator()" << std::endl;
-	std::cout << "=====================================" <<std::endl; 
-	std::cout << "Event = " << event.id().event() << ", Lumi Block = " << event.id().luminosityBlock() << std::endl;
-	std::cout << "=====================================" <<std::endl; 
+  std::cout << "Processing Event in TestEventSelector::operator()" << std::endl;
+  std::cout << "=====================================" <<std::endl;
+  std::cout << "Event = " << event.id().event() << ", Lumi Block = " << event.id().luminosityBlock() << std::endl;
+  std::cout << "=====================================" <<std::endl;
 
-    while(1){ // standard infinite while loop trick to avoid nested ifs
-    
-		passCut(ret, "No selection");
+  while(1){ // standard infinite while loop trick to avoid nested ifs
 
-		edm::Handle< edm::TriggerResults > triggersHandle;
-		event.getByToken(triggersToken,triggersHandle);
-		//event.getByLabel("TriggerResults::HLT", triggersHandle);
-		const edm::TriggerNames trigNames = event.triggerNames(*triggersHandle);
+    passCut(ret, "No selection");
 
-		unsigned int nTrig = triggersHandle->size();
-		bool passTrig = false;
-		for(std::vector<std::string>::const_iterator targetTrig = vTargetTrigs.begin(); targetTrig!= vTargetTrigs.end(); targetTrig++){
+    edm::Handle< edm::TriggerResults > triggersHandle;
+    event.getByToken(triggersToken,triggersHandle);
+    //event.getByLabel("TriggerResults::HLT", triggersHandle);
+    const edm::TriggerNames trigNames = event.triggerNames(*triggersHandle);
 
-			for(unsigned int i=0; i<nTrig;i++){
+    unsigned int nTrig = triggersHandle->size();
+    bool passTrig = false;
+    for(std::vector<std::string>::const_iterator targetTrig = vTargetTrigs.begin(); targetTrig!= vTargetTrigs.end(); targetTrig++){
 
-				std::string trigName = trigNames.triggerName(i);			
-				if(trigName.find((*targetTrig)) == std::string::npos) continue;
+      for(unsigned int i=0; i<nTrig;i++){
 
-				if(triggersHandle->accept(trigNames.triggerIndex(trigName))){
+	std::string trigName = trigNames.triggerName(i);
+	if(trigName.find((*targetTrig)) == std::string::npos) continue;
 
-					if(debug)std::cout << "\tFIRED : "<< (*targetTrig) << std::endl;		
-					
-					passTrig = true;
+	if(triggersHandle->accept(trigNames.triggerIndex(trigName))){
 
-				}
+	  if(debug)std::cout << "\tFIRED : "<< (*targetTrig) << std::endl;
 
-			}	
+	  passTrig = true;
 
-		}
+	}
 
-		if(passTrig)passCut(ret, "Trigger");
-		else break;
+      }
 
-		passCut(ret, "All cuts");
-        break;
+    }
 
-    } // end of while loop
+    if(passTrig)passCut(ret, "Trigger");
+    else break;
+
+    passCut(ret, "All cuts");
+    break;
+
+  } // end of while loop
 
 
-    bFirstEntry = false;
-    
-    return (bool)ret;
-  
-    setIgnored(ret);
+  bFirstEntry = false;
 
-    return false;
+  return (bool)ret;
+
+  setIgnored(ret);
+
+  return false;
 }// end of operator()
 
 
 void TestEventSelector::AnalyzeEvent( edm::EventBase const & event, LjmetEventContent & ec )
 {
-    //
-    // Compute analysis-specific quantities in the event,
-    // return via event content
-    //
+  //
+  // Compute analysis-specific quantities in the event,
+  // return via event content
+  //
 
-//   ec.SetValue("pi", 3.14);
-  
+  //   ec.SetValue("pi", 3.14);
 
-    return;
+
+  return;
 }
 
 void TestEventSelector::EndJob()
